12decc55e0cde579b18c6df7be7a965fb6ac6724
Alexander Bulychev <asbul@yandex.ru>
2022-06-30 06:18:03 +0300

    Configuring UIImagePickerController and PHPickerViewController

--- MyFavoritePlaces/NewPlaceTableViewController.swift
+++ MyFavoritePlaces/NewPlaceTableViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import PhotosUI
 
-class NewPlaceTableViewController: UITableViewController {
+final class NewPlaceTableViewController: UITableViewController {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +20,24 @@ class NewPlaceTableViewController: UITableViewController {
     // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(
+                title: nil,
+                message: nil,
+                preferredStyle: .actionSheet
+            )
 
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker()
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.choosePHPicker()
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
@@ -29,5 +49,27 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.becomeFirstResponder()
         return true
+    }
+}
+
+// MARK: - Work with Image
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate {
+    private func chooseImagePicker() {
+        let source = UIImagePickerController.SourceType.camera
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
+    }
+
+    private func choosePHPicker() {
+        let configuration = PHPickerConfiguration()
+        let phPicker = PHPickerViewController(configuration: configuration)
+        present(phPicker, animated: true)
     }
 }
--- MyFavoritePlaces/TableViewController.swift
+++ MyFavoritePlaces/TableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+final class TableViewController: UITableViewController {
 
     let places = Place.getPlaces()
 
