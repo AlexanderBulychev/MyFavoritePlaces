eb2326dfa5d2408219887d7c85b0e0ec6f2ae325
Alexander Bulychev <asbul@yandex.ru>
2022-07-01 08:55:57 +0300

    Action Sheet Customization

--- MyFavoritePlaces/NewPlaceTableViewController.swift
+++ MyFavoritePlaces/NewPlaceTableViewController.swift
@@ -26,12 +26,21 @@ final class NewPlaceTableViewController: UITableViewController {
                 preferredStyle: .actionSheet
             )
 
+            let photoIcon = #imageLiteral(resourceName: "camera")
+            let imageIcon = #imageLiteral(resourceName: "photo")
+
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseImagePicker()
             }
+            camera.setValue(photoIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.choosePHPicker()
             }
+            photo.setValue(imageIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
 
             actionSheet.addAction(camera)
@@ -52,24 +61,51 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
     }
 }
 
-// MARK: - Work with Image
-extension NewPlaceTableViewController: UIImagePickerControllerDelegate {
+// MARK: - Work with Photo
+
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     private func chooseImagePicker() {
         let source = UIImagePickerController.SourceType.camera
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        <#code#>
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+
     }
+}
+
+// MARK: - Work with Images
+
+extension NewPlaceTableViewController: PHPickerViewControllerDelegate {
 
     private func choosePHPicker() {
         let configuration = PHPickerConfiguration()
         let phPicker = PHPickerViewController(configuration: configuration)
+        phPicker.delegate = self
         present(phPicker, animated: true)
+    }
+
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        for result in results {
+            result.itemProvider.loadObject(ofClass: UIImage.self) { (object, error) in
+                if let image = object as? UIImage {
+                    DispatchQueue.main.async {
+                        self.imageOfPlace.image = image
+                        self.imageOfPlace.contentMode = .scaleAspectFill
+                        self.imageOfPlace.clipsToBounds = true
+                    }
+                }
+            }
+        }
+        dismiss(animated: true)
     }
 }
